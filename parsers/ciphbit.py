d505ca40b9838da36e2bf7455a8013f22ead7268
mkey7 <houjiang123456@sina.com>
2024-05-16 11:09:48 +0800

    ciphbit

--- parsers/ciphbit.py
+++ parsers/ciphbit.py
@@ -13,18 +13,19 @@ from sharedutils import errlog, find_slug_by_md5, extract_md5_from_filename, rem
 from parse import appender
 from datetime import datetime
 
+# TODO 完成该组织的分析
+
 # Define a function to convert the date format
-def convert_date_format(input_date):
+def convert_date_format(date):
     # Split the input date string to extract the month, day, and year
-    parts = input_date.split(', ')
-    if len(parts) > 2:
-        date_str = parts[1] + ', ' + parts[2]  # Extract "April 25, 2023" part
-        # Parse the date using datetime
-        parsed_date = datetime.strptime(date_str, '%B %d, %Y')
-        # Format the date in the desired format
-        formatted_date = parsed_date.strftime('%Y-%m-%d %H:%M:%S.%f')
-        return formatted_date
-    return input_date  # Return the input date if the 
+    dates = date.split(', ')
+    date_string = dates[1]+', '+dates[2]
+    
+    date_format = "%b %d, %Y"
+
+    date_object = datetime.strptime(date_string, date_format)
+
+    return date_object
 
 def main():
     for filename in os.listdir('source'):
@@ -32,23 +33,26 @@ def main():
             if filename.startswith('ciphbit-'):
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
+                post_url = "http://ciphbitqyg26jor7eeo6xieyq7reouctefrompp6ogvhqjba7uo4xdid.onion/"
                 soup=BeautifulSoup(file,'html.parser')
-                h2_elements = soup.find_all('h2')
-                for h2 in h2_elements:
-                    victim = h2.get_text().replace('\n','')
-                    a_element = h2.find_next('a')
-                    website = a_element.get('href') if a_element else ''
-                    h5_element = h2.find_next('h5')
-                    # Get the text within the <h5> element as the published date
+                row = soup.find('div', class_="row")
+                post_elements = row.find_all("div",class_="post")
+                for element in post_elements:
+                    h2_element = element.find("h2")
+                    website = h2_element.find('a')['href'] if h2_element.find('a') else ''
+                    title = h2_element.get_text()
+                    description = element.find('div').get_text()
+                    h5 = element.find_all('h5')
+                    date = h5[0].get_text()
                     try:
-                        published_date = h5_element.get_text() if h5_element else ''
-                        published = convert_date_format(published_date)
-                    except: 
+                        published = convert_date_format(date)
+                    except:
                         published = ''
-                    p_elements = h2.find_parent().find_all('p')
-                    description = ' '.join(p.get_text() for p in p_elements)
-                    description = description.replace('\n','')
-                    appender(remove_multiple_spaces(victim), 'ciphbit', remove_multiple_spaces(description),website,published)
+                    
+                    down = post_url + h5[1].find('a')['id']
+
+                #     # TODO description内容缺失
+                    appender(title, 'ciphbit', description,website,published,post_url,download=down)
                 file.close()
         except:
             errlog("Failed during : " + filename)
